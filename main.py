5f3a11be4536ea17b41f4f1e68bc785f239009b8
Jinesh Mehta <mehta.jinesh@learner.manipal.edu>
2019-10-10 01:07:39 +0530

    feat:  processing data and saving into train.csv

--- main.py
+++ main.py
@@ -2,6 +2,25 @@
 import os
 import glob
 
+#for shuffling postive and negative reviews
+from sklearn.utils import shuffle
+
+#for removing stop words and stemming the words in review
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import re
+from bs4 import BeautifulSoup
+
+#caching the preprocessed reviews
+import pickle
+
+#storing preprocess words in numpy array
+import numpy as np
+
+#store the processed data into train.csv using dataframe
+import pandas as pd
+
 def read_imdb_data(data_dir='data\\aclImdb'):
     """Reading imdb data from the files"""
     
@@ -29,7 +48,146 @@ def read_imdb_data(data_dir='data\\aclImdb'):
                 
     return data, labels
 
+def prepare_imdb_data(data, labels):
+    """Prepare training and test sets from IMDb movie reviews."""
+    
+    #Combine positive and negative reviews and labels
+    data_train = data['train']['pos'] + data['train']['neg']
+    data_test = data['test']['pos'] + data['test']['neg']
+    labels_train = labels['train']['pos'] + labels['train']['neg']
+    labels_test = labels['test']['pos'] + labels['test']['neg']
+    
+    #Shuffle reviews and corresponding labels within training and test sets
+    data_train, labels_train = shuffle(data_train, labels_train)
+    data_test, labels_test = shuffle(data_test, labels_test)
+    
+    # Return a unified training data, test data, training labels, test labets
+    return data_train, data_test, labels_train, labels_test
+
+def review_to_words(review):
+    """Converting the review to meaningful words by stemming unneccessary information."""
+    
+    nltk.download("stopwords", quiet=True)
+    stemmer = PorterStemmer()
+    
+    text = BeautifulSoup(review, "html.parser").get_text() # Remove HTML tags
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) # Convert to lower case
+    words = text.split() # Split string into words
+    words = [w for w in words if w not in stopwords.words("english")] # Remove stopwords
+    words = [stemmer.stem(w) for w in words] # stem
+    
+    return words
+
+def preprocess_data(data_train, data_test, labels_train, labels_test,
+                    cache_dir, cache_file="preprocessed_data.pkl"):
+    """Convert each review to words; read from cache if available."""
+
+    # If cache_file is not None, try to read from it first
+    cache_data = None
+    if cache_file is not None:
+        try:
+            with open(os.path.join(cache_dir, cache_file), "rb") as f:
+                cache_data = pickle.load(f)
+            print("Read preprocessed data from cache file:", cache_file)
+        except:
+            pass  # unable to read from cache, but that's okay
+    
+    # If cache is missing, then do the heavy lifting
+    if cache_data is None:
+        print('Working on it.')
+        # Preprocess training and test data to obtain words for each review
+        #words_train = list(map(review_to_words, data_train))
+        #words_test = list(map(review_to_words, data_test))
+        words_train = [review_to_words(review) for review in data_train]
+        print('Training Done.')
+        words_test = [review_to_words(review) for review in data_test]
+        print('Testing Done.')        
+        # Write to cache file for future runs
+        if cache_file is not None:
+            cache_data = dict(words_train=words_train, words_test=words_test,
+                              labels_train=labels_train, labels_test=labels_test)
+            with open(os.path.join(cache_dir, cache_file), "wb") as f:
+                pickle.dump(cache_data, f)
+            print("Wrote preprocessed data to cache file:", cache_file)
+    else:
+        # Unpack data loaded from cache file
+        words_train, words_test, labels_train, labels_test = (cache_data['words_train'],
+                cache_data['words_test'], cache_data['labels_train'], cache_data['labels_test'])
+    
+    return words_train, words_test, labels_train, labels_test
+
+def build_dict(data, vocab_size = 5000):
+    """Construct and return a dictionary mapping each of the most frequently appearing words to a unique integer."""
+    
+    # Determine how often each word appears in `data`. Note that `data` is a list of sentences and that a
+    #       sentence is a list of words.
+
+    word_count = {} # A dict storing the words that appear in the reviews along with how often they occur
+    
+    for review in data:
+        for word in review:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+    # Sort the words found in `data` so that sorted_words[0] is the most frequently appearing word and
+    #       sorted_words[-1] is the least frequently appearing word.
+    
+    sorted_words = [item[0] for item in sorted(word_count.items(), key=lambda x: x[1], reverse=True)]
+    
+    word_dict = {} # This is what we are building, a dictionary that translates words into integers
+    for idx, word in enumerate(sorted_words[:vocab_size - 2]): # The -2 is so that we save room for the 'no word'
+        word_dict[word] = idx + 2                              # 'infrequent' labels
+        
+    return word_dict
+
+def convert_and_pad(word_dict, sentence, pad=500):
+    """Mapping words to integers and adding extra 0 if the review size < 500"""
+    
+    NOWORD = 0 # We will use 0 to represent the 'no word' category
+    INFREQ = 1 # and we use 1 to represent the infrequent words, i.e., words not appearing in word_dict
+    
+    working_sentence = [NOWORD] * pad
+    
+    for word_index, word in enumerate(sentence[:pad]):
+        if word in word_dict:
+            working_sentence[word_index] = word_dict[word]
+        else:
+            working_sentence[word_index] = INFREQ
+            
+    return working_sentence, min(len(sentence), pad)
+
+def convert_and_pad_data(word_dict, data, pad=500):
+    result = []
+    lengths = []
+    
+    for sentence in data:
+        converted, leng = convert_and_pad(word_dict, sentence, pad)
+        result.append(converted)
+        lengths.append(leng)
+        
+    return np.array(result), np.array(lengths)
+
 def main():
     data, labels = read_imdb_data()
-
+    train_X, test_X, train_y, test_y = prepare_imdb_data(data, labels)
+    #storing the preprocess data as cache
+    cache_dir = os.path.join("cache", "sentiment_analysis")  # where to store cache files
+    os.makedirs(cache_dir, exist_ok=True)  # ensure cache directory exists
+    # Preprocess data
+    train_X, test_X, train_y, test_y = preprocess_data(train_X, test_X, train_y, test_y,cache_dir)
+    #building word dict from reviews
+    word_dict = build_dict(train_X)
+    #now we store word dict for future references
+    data_dir = 'data/pytorch' # The folder we will use for storing data
+    if not os.path.exists(data_dir): # Make sure that the folder exists
+        os.makedirs(data_dir)
+    with open(os.path.join(data_dir, 'word_dict.pkl'), "wb") as f:
+        pickle.dump(word_dict, f)
+    train_X, train_X_len = convert_and_pad_data(word_dict, train_X)
+    test_X, test_X_len = convert_and_pad_data(word_dict, test_X)
+    #store processed data
+    pd.concat([pd.DataFrame(train_y), pd.DataFrame(train_X_len), pd.DataFrame(train_X)], axis=1) \
+        .to_csv(os.path.join(data_dir, 'train.csv'), header=False, index=False)
+    return
 main()
