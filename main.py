18605b05f36a7f2308f422775967af54b4e933fa
Jinesh Mehta <mehta.jinesh@learner.manipal.edu>
2019-10-10 04:55:45 +0530

    feat: deploy for prediction added
    added the function to create a model which will be alive in awsSagemaker

--- main.py
+++ main.py
@@ -21,6 +21,29 @@ import numpy as np
 #store the processed data into train.csv using dataframe
 import pandas as pd
 
+#training uploading train data to s3
+import sagemaker
+import boto3
+
+#for loading env variable
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+#training model using pytorch
+import torch
+import torch.utils.data
+
+import torch.optim as optim
+#for import train module
+import sys
+sys.path.insert(0, 'train/')
+from model import LSTMClassifier
+from train import train
+from sagemaker.pytorch import PyTorch
+from sklearn.metrics import accuracy_score
+from sagemaker.predictor import RealTimePredictor
+from sagemaker.pytorch import PyTorchModel
+
 def read_imdb_data(data_dir='data\\aclImdb'):
     """Reading imdb data from the files"""
     
@@ -168,6 +191,25 @@ def convert_and_pad_data(word_dict, data, pad=500):
         
     return np.array(result), np.array(lengths)
 
+def loadEnv():
+    """ 
+    Loads the enviroment vars like access key info
+    to the file 
+    """
+    # Create .env file path.
+    dotenv_path = join(dirname(__file__), '.env')
+    # Load file from the path.
+    load_dotenv(dotenv_path)
+    return
+
+def predict(pytorch_predictor, data, rows=512):
+    # We split the data into chunks and send each chunk seperately, accumulating the results.
+    split_array = np.array_split(data, int(data.shape[0] / float(rows) + 1))
+    predictions = np.array([])
+    for array in split_array:
+        predictions = np.append(predictions, pytorch_predictor.predict(array))
+    return predictions
+
 def main():
     data, labels = read_imdb_data()
     train_X, test_X, train_y, test_y = prepare_imdb_data(data, labels)
@@ -184,10 +226,75 @@ def main():
         os.makedirs(data_dir)
     with open(os.path.join(data_dir, 'word_dict.pkl'), "wb") as f:
         pickle.dump(word_dict, f)
+
     train_X, train_X_len = convert_and_pad_data(word_dict, train_X)
     test_X, test_X_len = convert_and_pad_data(word_dict, test_X)
+
     #store processed data
     pd.concat([pd.DataFrame(train_y), pd.DataFrame(train_X_len), pd.DataFrame(train_X)], axis=1) \
         .to_csv(os.path.join(data_dir, 'train.csv'), header=False, index=False)
+    
+    loadEnv()
+    # Accessing variables.
+    access_key_id = os.getenv('ACCESS_KEY_ID')
+    secret_key = os.getenv('SECRET_KEY')
+    region = os.getenv('AWS_REGION')
+    execution_role = os.getenv('EXEC_ROLE')
+    # create sagemaker session
+    session = boto3.Session(aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_key,region_name=region)
+
+    sagemaker_session = sagemaker.Session(boto_session=session)
+
+    #update data to s3 bucket
+    bucket = sagemaker_session.default_bucket()
+    prefix = 'sagemaker/sentiment_rnn'
+    role = execution_role
+    input_data = sagemaker_session.upload_data(path=data_dir, bucket=bucket, key_prefix=prefix)
+    
+    # Read in only the first 250 rows
+    train_sample = pd.read_csv(os.path.join(data_dir, 'train.csv'), header=None, names=None, nrows=250)
+
+    # Turn the input pandas dataframe into tensors
+    train_sample_y = torch.from_numpy(train_sample[[0]].values).float().squeeze()
+    train_sample_X = torch.from_numpy(train_sample.drop([0], axis=1).values).long()
+
+    # Build the dataset
+    train_sample_ds = torch.utils.data.TensorDataset(train_sample_X, train_sample_y)
+    # Build the dataloader
+    train_sample_dl = torch.utils.data.DataLoader(train_sample_ds, batch_size=50)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    lstm_model = LSTMClassifier(32, 100, 5000).to(device)
+    optimizer = optim.Adam(lstm_model.parameters())
+    loss_fn = torch.nn.BCELoss()
+
+    train(lstm_model, train_sample_dl, 5, optimizer, loss_fn, device)
+
+    estimator = PyTorch(entry_point="train.py",
+                    source_dir="train",
+                    role=role,
+                    framework_version='0.4.0',
+                    train_instance_count=1,
+                    train_instance_type='ml.m4.xlarge',
+                    hyperparameters={
+                        'epochs': 10,
+                        'hidden_dim': 200,
+                    })
+    estimator.fit({'training': input_data})
+
+    # Deploy the trained model
+    class StringPredictor(RealTimePredictor):
+        def __init__(self, endpoint_name, sagemaker_session):
+            super(StringPredictor, self).__init__(endpoint_name, sagemaker_session, content_type='text/plain')
+
+    py_model = PyTorchModel(model_data=estimator.model_data,
+                        role = role,
+                        framework_version='0.4.0',
+                        entry_point='predict.py',
+                        source_dir='serve',
+                        predictor_cls=StringPredictor)
+    pytorch_predictor = py_model.deploy(initial_instance_count=1, instance_type='ml.m4.xlarge')
+    print(pytorch_predictor.endpoint)
     return
 main()
